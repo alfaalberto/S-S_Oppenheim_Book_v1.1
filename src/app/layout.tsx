f4b3ec4a43d90d69b3ece06a099798ae8eff1e6e
Alberto <arosaless23@gmail.com>
2025-06-27 16:12:10 -0600

    Ajuste de estilos: fondo oscuro y modo dark para producción

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,14..72,400;0,14..72,700;1,14..72,400&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased bg-background text-foreground dark:bg-background dark:text-foreground">
         {children}
         <Toaster />
       </body>
